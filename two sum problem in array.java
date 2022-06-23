aa19ba5ac4e3780e9e2e4ca432a308eaec197f45
Nagilla Praveen Reddy <107873288+nprrgv@users.noreply.github.com>
2022-06-23 09:37:42 +0530

    Update two sum problem in array.java

--- two sum problem in array.java
+++ two sum problem in array.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+import java.lang.*;
+public class Solution {
+  public static void main(String args[]){
+    int[] nums={2,7,11,19};
+    int target=9;
+    System.out.println(Arrays.toString(twoSum(nums,target)));
+}
+    public static int[] twoSum(int[] nums, int target) {
+        int[] matched=new int[2];
+       // int j=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                       matched[0]=i;
+                       matched[1]=j;
+            }   
+            }
+        
+        }
+        return matched;
+        
+    }
+}
